2de97251026710a1bc0aa5d5cb325f411e9f27ef
agusakow <alexandergusakov1@gmail.com>
2016-06-21 00:25:01 +0300

    added blocks/button

--- src/blocks/button/button.jsx
+++ src/blocks/button/button.jsx
@@ -1,5 +1,20 @@
-var Button = React.createClass({
-      render: function() {
-        return <button>Hello {this.props.name}</button>;
-      }
-});
+import React from 'react';
+
+export default class extends React.Component{
+    render() {
+        var className = 'button';
+        var title = this.props.title;
+        var icon;
+        if (this.props.className) {
+            className += ` ${this.props.className}`;
+        }
+        if (this.props.icon) {
+            icon = <span className={`icon icon_${this.props.icon}`}/>
+        }
+        return (
+            <button {...this.props} className={className}>
+                {icon}
+            </button>
+        );
+    }
+};
