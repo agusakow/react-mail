3aa1706e6765027f4a6d7d929635b06f84d57a28
agusakow <alexandergusakov1@gmail.com>
2016-06-21 03:19:13 +0300

    rm comments

--- src/js/dispatcher.js
+++ src/js/dispatcher.js
@@ -2,8 +2,7 @@ import { Dispatcher } from 'flux';
 
 var dispatcher = new Dispatcher();
 dispatcher.dispatch = function() {
-    //console.log(arguments);
     Dispatcher.prototype.dispatch.apply(this, arguments);
-}
+};
 
 export default dispatcher;
--- src/js/page.js
+++ src/js/page.js
@@ -4,25 +4,12 @@ import appActions from '../actions/appActions';
 export default class Page extends React.Component {
     constructor(params) {
         super(params);
-        //this.loading = true;
-    }
-
-    setState(state) {
-        //this.loading = false;
-        //this.setLoading(false);
-        super.setState(state);
-    }
-
-    setLoading(state) {
-        //setTimeout(() => appActions.setLoading(state), 1);
     }
 
     componentDidMount() {
         if (this.onCreate) {
             this.onCreate();
         }
-        //this.setLoading(true);
-        //setTimeout(() => this.fetch(this.props), 1);
     }
 
     componentWillUnmount() {
@@ -31,18 +18,7 @@ export default class Page extends React.Component {
         }
     }
 
-    componentWillReceiveProps(nextProps) {
-        //if (nextProps.params.id != this.props.params.id) {
-        //    this.setLoading(true);
-        //    this.fetch(nextProps);
-        //}
-    }
-
     render() {
-        if (this.loading) {
-            return null;
-        } else {
-            return this.getDom();
-        }
+        return this.getDom();
     }
 };
